81dbfb3d3b0c3123e3dac2447efb6f91cdf73d2a
Chris White <cwhite@defaqto.com>
2016-11-15 13:50:40 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = SetupConfiguration();
+
+            System.Console.WriteLine("Configuration Manager");
+            string spliter = "===============================\r\n";
+            System.Console.WriteLine(spliter);
+
+            // Run through all keys
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine("Key: {0} = {1}", item.Key, item.Value);
+            }
+
+        }
+
+        private static IConfigurationRoot SetupConfiguration() {
+            
+            var builder = new ConfigurationBuilder();
+            var env = PlatformServices.Default.Application;
+
+            // Configuration can come from being In Memory or from File
+            builder.SetBasePath(System.IO.Directory.GetCurrentDirectory());
+
+            builder.AddInMemoryCollection(); // Memory Configuration
+            builder.AddJsonFile("appSettings.json");
+            builder.AddJsonFile($"appSettings.{System.Environment.GetEnvironmentVariable("EnvironmentName")}.json", optional: true);
+
+            var config = builder.Build();
+
+
+            config["someKey"] = "someValue";
+
+            return config;
+        }
+    }
+}
